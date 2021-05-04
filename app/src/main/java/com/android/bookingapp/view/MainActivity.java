1f5d0dd3e56c5710b470c778d264f09c97f73091
Le Nghia <nghja1832000@gmail.com>
2021-05-04 22:12:29 +0700

    done model

--- app/src/main/java/com/android/bookingapp/view/MainActivity.java
+++ app/src/main/java/com/android/bookingapp/view/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.android.bookingapp.R;
+import com.android.bookingapp.model.Date;
+import com.android.bookingapp.model.User;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        User user = new User(2, "abc","abc","abc","abc",new Date("18","03","2000"), true, "abc", "abc");
+        DatabaseReference myRef = database.getReference("User");
+        myRef.child("User"+user.getId()).setValue(user);;
     }
 }
