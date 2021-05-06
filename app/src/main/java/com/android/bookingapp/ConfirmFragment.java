9a4529d0a0b628bc83e0f466603b0283ef11385c
Le Nghia <nghja1832000@gmail.com>
2021-05-06 21:51:36 +0700

    done register

--- app/src/main/java/com/android/bookingapp/ConfirmFragment.java
+++ app/src/main/java/com/android/bookingapp/ConfirmFragment.java
@@ -1,64 +1,72 @@
 package com.android.bookingapp;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ConfirmFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.android.bookingapp.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class ConfirmFragment extends Fragment {
+    private User user;
+    private Button bt_done;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ConfirmFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment confrimFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ConfirmFragment newInstance(String param1, String param2) {
-        ConfirmFragment fragment = new ConfirmFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            user=(User)getArguments().getSerializable("user");
         }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+
+        bt_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.child("User").child("User"+user.getId()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(getContext(),"Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(getContext(),"Lỗi", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_confrim, container, false);
+        View view= inflater.inflate(R.layout.fragment_confirm, container, false);
+        bt_done=view.findViewById(R.id.bt_done);
+        return view;
     }
+
 }
--- app/src/main/java/com/android/bookingapp/moreInfoFragment.java
+++ app/src/main/java/com/android/bookingapp/moreInfoFragment.java
@@ -1,64 +1,145 @@
 package com.android.bookingapp;
 
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link moreInfoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class moreInfoFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import com.android.bookingapp.model.Date;
+import com.android.bookingapp.model.User;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+import java.util.ArrayList;
 
-    public moreInfoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment moreInfoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static moreInfoFragment newInstance(String param1, String param2) {
-        moreInfoFragment fragment = new moreInfoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class moreInfoFragment extends Fragment  {
+    private User user;
+    private EditText edt_job,edt_address;
+    private Button bt_submit;
+    RadioButton male;
+    Spinner spinner_day,spinner_month,spinner_year;
+    private String nDay,nMonth,nYear;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            user=(User)getArguments().getSerializable("user");
         }
+    }
+    public void addItemsOnSpinner() {
+
+        ArrayList<Integer> day = new ArrayList<>();
+        ArrayList<Integer> month = new ArrayList<>();
+        ArrayList<Integer> year = new ArrayList<>();
+        for(int i=1;i<32;i++)
+        {
+            if(i<13) month.add(i);
+            day.add(i);
+        }
+        for(int i=1930;i<2021;i++)
+        {
+            year.add(i);
+        }
+        ArrayAdapter<Integer> dataAdapter = new ArrayAdapter<Integer>((Context) getActivity(),
+                android.R.layout.simple_spinner_item, day);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_day.setAdapter(dataAdapter);
+        spinner_day.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                nDay= parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<Integer> dataAdapter2 = new ArrayAdapter<Integer>((Context) getActivity(),
+                android.R.layout.simple_spinner_item, month);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_month.setAdapter(dataAdapter2);
+        spinner_month.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                nMonth= parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<Integer> dataAdapter3 = new ArrayAdapter<Integer>((Context) getActivity(),
+                android.R.layout.simple_spinner_item, year);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_year.setAdapter(dataAdapter3);
+        spinner_year.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                nYear= parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        addItemsOnSpinner();
+        bt_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Xử lý xác thực gmail
+                User user1=new User(user.getId(),user.getEmail(),user.getPassword(),user.getFullname(),user.getPhone(),
+                        new Date(nDay,nMonth,nYear),male.isChecked(),edt_job.getText().toString(),edt_address.getText().toString());
+                if(!edt_job.getText().toString().isEmpty()&&!edt_address.getText().toString().isEmpty())
+                {
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable("user",user1);
+                    Navigation.findNavController(v).navigate(R.id.action_moreInfoFragment_to_confrimFragment, bundle);
+                }
+                else
+                {
+                    Toast.makeText(getActivity(),"Vui lòng điền đủ thông tin",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more_info, container, false);
+        View view= inflater.inflate(R.layout.fragment_more_info, container, false);
+        edt_job=view.findViewById(R.id.job);
+        edt_address=view.findViewById(R.id.address);
+        male=view.findViewById(R.id.male);
+        bt_submit=view.findViewById(R.id.bt_submit);
+        spinner_day = (Spinner) view.findViewById(R.id.spinner_date);
+        spinner_month = (Spinner) view.findViewById(R.id.spinner_month);
+        spinner_year = (Spinner) view.findViewById(R.id.spinner_year);
+        return view;
     }
+
 }
